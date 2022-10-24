692c57ad0bbece0024ad5070b3c54921654aa57f
luucongthanh <congthanhluu@gmail.com>
2022-10-24 15:38:06 +0700

    fix: base layout overflow

--- src/container/BaseLayout/BaseLayout.tsx
+++ src/container/BaseLayout/BaseLayout.tsx
@@ -10,7 +10,7 @@ const BaseLayout: FC<IProps> = ({ children }) => {
   return (
     <Flex minHeight="100vh" maxHeight="100vh" direction="row" className="base-layout">
       <SideBar />
-      <Box flex={1} p="6" bgColor="gray.50">
+      <Box flex={1} p="6" bgColor="gray.50" maxHeight="100vh" overflowY="auto">
         {children}
       </Box>
     </Flex>
